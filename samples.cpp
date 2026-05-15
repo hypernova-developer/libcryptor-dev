25a02bfc4ffb531e6270e15dc766cc1e7ce7798e
卄ㄚ卩乇尺几ㄖᐯ卂✨ <mhmmdeymngrbz@gmail.com>
2026-05-15 23:09:42 +0300

    Update samples.cpp

--- samples.cpp
+++ samples.cpp
@@ -1,1 +1,14 @@
+#include "cryptor.hpp"
+#include <iostream>
 
+int main()
+{
+    std::string raw = "hypernova-developer";
+    
+    std::string locked = cryptor::encryptor::process(cryptor::Method::ASCII_SHIFT, raw);
+    
+    std::string unlocked = cryptor::decryptor::process(cryptor::Method::ASCII_SHIFT, locked);
+
+    std::cout << "Locked: " << locked << "\nUnlocked: " << unlocked << std::endl;
+    return 0;
+}
